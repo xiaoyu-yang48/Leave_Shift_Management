c80c66c1b3174577f8390fcfe215bcfa3baa3e52
Xiaoyu <xiaoyuyang48@gmail.com>
2025-08-07 14:26:38 +1000

    front end design on homepage shiftswap page and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,17 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
 
+import WorkHome from './pages/WorkHome';
+import ShiftSwap from './pages/ShiftSwap';
+import Leave from './pages/Leave';
+import Overtime from './pages/Overtime';
+import RequestStatus from './pages/RequestStatus';
+
+import AdminHome from './pages/AdminHome';
+import AdminShift from './pages/AdminShift';
+import AdminLeave from './pages/AdminLeave'; 
+import AdminOvertime from './pages/AdminOvertime';
+
 function App() {
   return (
     <Router>
@@ -14,6 +25,19 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
+        {/* employee pages */}
+        <Route path="/home" element={<WorkHome />} />
+        <Route path="/shiftswap/:shiftId" element={<ShiftSwap />} />
+        <Route path="/leave" element={<Leave />} />
+        <Route path="/overtime" element={<Overtime />} />
+        <Route path="/request_status" element={<RequestStatus />} />
+
+        {/* admin pages */} 
+        <Route path="/admin_home" element={<AdminHome />} />
+        <Route path="/admin_shift" element={<AdminShift />} />
+        <Route path="/admin_leave" element={<AdminLeave />} />
+        <Route path="/admin_overtime" element={<AdminOvertime />} />
+
       </Routes>
     </Router>
   );
