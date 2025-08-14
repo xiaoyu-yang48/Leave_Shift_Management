d0cb8dd6c6254576df7f460adac277bd8c683546
Xiaoyu <xiaoyuyang48@gmail.com>
2025-08-15 00:55:25 +1000

    LSMS-129 User Story 2.2: View Personal Schedule

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 //app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/schedule', require('./routes/workhomeRoutes'));
 
 // Export the app object for testing
 if (require.main === module) {
