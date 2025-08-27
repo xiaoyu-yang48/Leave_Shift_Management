6bd5655b64a4af162ffd24c3a2b0b0a4711c92dd
Xiaoyu <xiaoyuyang48@gmail.com>
2025-08-27 20:13:56 +1000

    token configuration

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://localhost:5001', // local
   // baseURL: 'http://54.66.59.106:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  });
 
 export default axiosInstance;
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, use, useEffect } from 'react';
-import axiosInstance from '../api/axiosInstance';
+import axiosInstance from '../axiosConfig';
 
 const AuthContext = createContext();
 
